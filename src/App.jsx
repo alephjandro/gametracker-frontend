74436ca18b98ad4a24b9c978c5328c52f986f3dc
Alejandro <alejobomu@gmail.com>
2025-10-29 21:11:39 -0500

    Actualizo carpeta gametracker-frontend eliminando y volviendo a instalar vite con sus componentes. Además, recalco que al haber creado un proyecto con anterioridad, es importante cambiarle el nombre de los archivos para poder visualizarlos en local host y no haya interferencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import BibliotecadeJuegos from "./components/Bibliotecadejuegos"
+
+
+
 function App() {
-    return (
-        <div>
-           <h1> Hello GameTracker </h1> 
-           <p>Tu biblioteca personal de videojuegos</p>
-        </div>    
-    )
+  return (
+    <div>
+      <h1>GameTracker</h1>
+      <BibliotecadeJuegos />
+    <p>Tu biblioteca personal de videojuegos</p> 
+  </div>
+)
 }
+
 export default App
---
+++ src/components/BibliotecadeJuegos.jsx
@@ -0,0 +1,4 @@
+function BibliotecadeJuegos() {
+    return <h2>Biblioteca de Juegos</h2>
+}
+export default BibliotecadeJuegos;
