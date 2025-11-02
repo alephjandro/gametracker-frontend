a37786059040b2b3d14c6edcf309a2f264ddeed3
aleph_jandro <alephbolanosmunoz@gmail.com>
2025-11-02 16:47:13 -0500

    Agregué una biblioteca de Java llamada axios que permite hacer solicitudes HTTP, pudiendo visualizar mi primer juego en React

--- src/components/BibliotecaJuegos.jsx
+++ src/components/BibliotecaJuegos.jsx
@@ -1,4 +1,29 @@
-function BibliotecadeJuegos() {
-    return <h2>Biblioteca de Juegos</h2>
+import { useEffect, useState } from 'react'
+import api from '../services/api'
+
+function BibliotecaJuegos() {
+  const [juegos, setJuegos] = useState([])
+
+  useEffect(() => {
+    api.get('/juegos')
+      .then(res => setJuegos(res.data))
+      .catch(err => console.error('Error al conectar con la API', err))
+  }, [])
+
+  return (
+    <div>
+      <h2>Biblioteca de Juegos</h2>
+      {juegos.length > 0 ? (
+        <ul>
+          {juegos.map(j => (
+            <li key={j._id}>{j.titulo}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No hay juegos registrados.</p>
+      )}
+    </div>
+  )
 }
-export default BibliotecadeJuegos;
+
+export default BibliotecaJuegos
